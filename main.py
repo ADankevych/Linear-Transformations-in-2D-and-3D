4e7b4bf2d2350b6de5242dbc07d7de2425197513
adankevych <adankevych@kse.org.ua>
2024-05-28 15:52:47 +0300

    rotating function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def rotate(object, angle):
+    angle = np.radians(angle)
+    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    return np.dot(object, rotation_matrix)
+
+
 batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
 star = np.array([[-0.5, 0], [0, 1.5], [0.5, 0], [-0.5, 1], [0.5, 1], [-0.5, 0]])
 
@@ -14,5 +21,11 @@ plt.ylim(-2, 2)
 plt.axhline(0, color='k', linestyle='-', linewidth=0.5)
 plt.axvline(0, color='k', linestyle='-', linewidth=0.5)
 plt.grid(True)
+
+rotate_star = rotate(star, 180)
+plt.plot(rotate_star[:, 0], rotate_star[:, 1], 'b')
+
+rotate_batman = rotate(batman, 90)
+plt.plot(rotate_batman[:, 0], rotate_batman[:, 1], 'g')
 
 plt.show()
