b1a66fd715a1d361230c67235602661c0b3d820d
adankevych <adankevych@kse.org.ua>
2024-06-02 02:12:01 +0300

    3d

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
 def rotate(object, angle):
@@ -71,6 +72,89 @@ def universal(object, rotate_angle, scale_coefficient, reflect_axis, tilt_angle,
     return object
 
 
+def rotate3d(object, angle, axis):
+    angle = np.radians(angle)
+    if axis == "x":
+        rotation_matrix = np.array([
+                                    [1, 0, 0],
+                                    [0, np.cos(angle), -np.sin(angle)],
+                                    [0, np.sin(angle),  np.cos(angle)]
+                                    ])
+    elif axis == "y":
+        rotation_matrix = np.array([
+                                    [np.cos(angle), 0, np.sin(angle)],
+                                    [0, 1, 0],
+                                    [-np.sin(angle), 0, np.cos(angle)]
+                                    ])
+    else:
+        rotation_matrix = np.array([
+                                    [np.cos(angle), -np.sin(angle), 0],
+                                    [np.sin(angle),  np.cos(angle), 0],
+                                    [0, 0, 1]
+                                    ])
+
+    print(np.dot(object, rotation_matrix))
+
+    return np.dot(object, rotation_matrix)
+
+
+def scale3d(object, coefficient, axis):
+    if axis == "x":
+        scale_matrix = np.array([
+                                [coefficient, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 1]
+                                ])
+    elif axis == "y":
+        scale_matrix = np.array([
+                                [1, 0, 0],
+                                [0, coefficient, 0],
+                                [0, 0, 1]
+                                ])
+    else:
+        scale_matrix = np.array([
+                                [1, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, coefficient]
+                                ])
+
+    print(np.dot(object, scale_matrix))
+
+    return np.dot(object, scale_matrix)
+
+
+def reflect3d(object, mirror_axis):
+    if mirror_axis == "x":
+        reflect_matrix = np.array([
+                                  [1, 0, 0],
+                                  [0, -1, 0],
+                                  [0, 0, -1]
+                                  ])
+    elif mirror_axis == "y":
+        reflect_matrix = np.array([
+                                  [-1, 0, 0],
+                                  [0, 1, 0],
+                                  [0, 0, -1]
+                                  ])
+    else:
+        reflect_matrix = np.array([
+                                  [-1, 0, 0],
+                                  [0, -1, 0],
+                                  [0, 0, 1]
+                                  ])
+
+    print(np.dot(object, reflect_matrix))
+
+    return np.dot(object, reflect_matrix)
+
+
+pyramid = np.array([
+                    [0,   0,   0],
+                    [1,   0,   0],
+                    [1,   1,   0],
+                    [0,   1,   0],
+                    [0.5, 0.5, 1]
+                    ])
 batman = np.array([
                    [0,       0],
                    [1,     0.2],
@@ -89,8 +173,7 @@ star = np.array([
                  [0,     1],
                  [1,     1],
                  [0,     0]
-                 ])           # подібний запис тільки для зручності сприйняття матриці
-
+                 ])
 plt.figure()
 
 plt.plot(batman[:, 0], batman[:, 1], 'b')
@@ -131,6 +214,49 @@ plt.grid(True)
 
 # universal_star = universal(star, 180, 0.5, "y", 15, "y")
 # plt.plot(universal_star[:, 0], universal_star[:, 1], 'g')
+
+
+plt.show()
+
+
+faces = [
+    [pyramid[j] for j in [0, 1, 4]],
+    [pyramid[j] for j in [1, 2, 4]],
+    [pyramid[j] for j in [2, 3, 4]],
+    [pyramid[j] for j in [3, 0, 4]],
+    [pyramid[j] for j in [0, 1, 2, 3]]
+]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.add_collection3d(Poly3DCollection(faces, facecolors='g', linewidths=1, edgecolors='g', alpha=0.5))
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_xlim(-1.5, 1.5)
+ax.set_ylim(-1.5, 1.5)
+ax.set_zlim(-1.5, 1.5)
+
+rotate_pyramid = rotate3d(pyramid, 90, "x")
+faces = [
+    [rotate_pyramid[j] for j in [0, 1, 4]],
+    [rotate_pyramid[j] for j in [1, 2, 4]],
+    [rotate_pyramid[j] for j in [2, 3, 4]],
+    [rotate_pyramid[j] for j in [3, 0, 4]],
+    [rotate_pyramid[j] for j in [0, 1, 2, 3]]
+]
+ax.add_collection3d(Poly3DCollection(faces, facecolors='b', linewidths=1, edgecolors='b', alpha=0.5))
+
+scale_pyramid = scale3d(pyramid, 2, "z")
+faces = [
+    [scale_pyramid[j] for j in [0, 1, 4]],
+    [scale_pyramid[j] for j in [1, 2, 4]],
+    [scale_pyramid[j] for j in [2, 3, 4]],
+    [scale_pyramid[j] for j in [3, 0, 4]],
+    [scale_pyramid[j] for j in [0, 1, 2, 3]]
+]
+ax.add_collection3d(Poly3DCollection(faces, facecolors='r', linewidths=1, edgecolors='r', alpha=0.5))
 
 
 plt.show()
