a8d01d520adbe9f33f613e9b5145291c3260c9fb
adankevych <adankevych@kse.org.ua>
2024-06-02 22:37:17 +0300

    part 2 scale

--- part2.py
+++ part2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage import zoom
+
 batman = np.array([
     [0, 0],
     [1, 0.2],
@@ -17,6 +19,9 @@ plt.figure()
 rotate_matrix = np.rot90(batman, 2)
 plt.plot(batman[:, 0], batman[:, 1], 'b')
 plt.plot(rotate_matrix[:, 0], rotate_matrix[:, 1], 'r')
+
+scaled_matrix = zoom(batman, [1.5, 1.5])
+plt.plot(scaled_matrix[:, 0], scaled_matrix[:, 1], 'g')
 
 plt.xlim(-2, 2)
 plt.ylim(-2, 2)
